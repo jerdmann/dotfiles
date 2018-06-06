d2b6ad150fe53b495ded1c3b4a868030a5d722c8
Jason Erdmann <jason.erdmann@tradingtechnologies.com>
2018-06-05 21:18:03 -0500

    much better

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
-sudo apt-get -y install openjdk-7-jre g++-multilib git vim-gtk stow zsh ctags silversearcher-ag python3 lib32z1 libreadline-dev libncurses5-dev libpcre3-dev libssl-dev perl make build-essential libc6:i386 libgl1-mesa-glx-lts-trusty:i386 valgrind tmux xclip jq linux-tools-generic linux-tools-4.4.0-72-generic
+packages=(
+build-essential
+ctags
+g++-multilib
+git
+jq
+lib32z1
+libc6:i386
+libgl1-mesa-glx-lts-trusty:i386
+libncurses5-dev
+libpcre3-dev
+libreadline-dev
+libssl-dev
+linux-tools-4.4.0-72-generic
+linux-tools-generic
+make
+openjdk-7-jre
+perl
+python3
+silversearcher-ag
+stow
+tmux
+valgrind
+vim-gtk
+xclip
+zsh
+)
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+sudo apt-get -y install "${packages[@]}"
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
