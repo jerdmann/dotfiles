8686e00932d5c3d0cbb3fb9a51eeeceeeac224d6
Jason Erdmann <jason.erdmann@tradingtechnologies.com>
2019-07-16 12:55:41 -0500

    help, terse

--- common/build.sh
+++ common/build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# faster debesys builds.  to use:
+# 1) populate ~/.my_mks with a list of mkfiles, one per line
+#    for example:
+#    some/project/build.mk
+#    other/project/build.mk
+# 2) run this script from anywhere in the repo like:
+#    > ~/build.sh some_target other_target
+
 rr=$(git rev-parse --show-toplevel)
 if [[ "$rr" == "" ]]; then
     echo "error: no repo"
@@ -10,10 +18,18 @@ if [[ "$#" -eq 0 ]]; then
     exit -1
 fi
 
-pushd $rr >/dev/null
-my_mks=$(for f in $(cat ~/.my_mks); do [[ -f "$f" ]] && printf "%s " $f; done)
-#make -j$(nproc) use_distcc=0 def_files="$my_mks" $@ 2>&1 | grep --line-buffered --color=never error:
-make -j$(nproc) use_distcc=0 def_files="$my_mks" $@ 2>&1 | grep --line-buffered --color=never -v -e "more than once" -e "^setting"
+terse=""
+if [[ "$1" == "--terse" ]]; then
+    terse="yes"
+    shift 1
+fi
+
+my_mks=$(for f in $(cat ~/.my_mks); do [[ -f "$rr/$f" ]] && printf "%s " $f; done)
+if [[ -z "$terse" ]]; then
+    make -C $rr -j$(nproc) use_distcc=0 def_files="$my_mks" $@ 2>&1
+else
+    make -C $rr -j$(nproc) use_distcc=0 def_files="$my_mks" $@ 2>&1 | grep --line-buffered --color=never error:
+fi
 rc=$?
-popd >/dev/null
 exit $rc
+
